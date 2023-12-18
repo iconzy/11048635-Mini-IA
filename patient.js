088c9ba441f00a1ce5bd4e94b29af874227ba193
Mandy Serwaa Acheampong <124740701+iconzy@users.noreply.github.com>
2023-12-17 21:12:44 -1200

    new changes
    new changes

---
+++ patient.js
@@ -0,0 +1,12 @@
+{
+    "surname": "string",
+    "otherNames": "string",
+    "gender": "string",
+    "phone": "string",
+    "address": "string",
+    "emergencyContact": {
+      "name": "string",
+      "phone": "string",
+      "relationship": "string"
+    }
+  }
