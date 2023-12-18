b2bc5d87a9c0b3b533181c0ae82e857337d7800f
Mandy Serwaa Acheampong <124740701+iconzy@users.noreply.github.com>
2023-12-17 21:27:19 -1200

    Add files via upload
    added new codes

---
+++ submit vitals.js
@@ -0,0 +1,12 @@
+{
+    "encounterId": "abcdef123456",
+    "bloodPressure": "120/80",
+    "temperature": "98.6",
+    "pulse": "75",
+    "spO2": "98"
+  }
+
+  {
+    "message": "Vitals submitted successfully"
+  }
+  
